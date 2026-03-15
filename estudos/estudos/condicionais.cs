a4daaf24194b681e2761bdb3e6ea5f5c287b43d0
GutLuiz <gutfoda@gmail.com>
2026-03-15 18:48:58 -0300

    condicionais

---
+++ estudos/estudos/condicionais.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos
+{
+    internal class condicionais
+    {
+        /*
+          -> Condicionais:
+           = É o código tomando decisões. Dependendo de uma condição, ele segue um caminho ou outro.
+
+          - estrutura:
+           =  if(codicao)
+              {
+               executa se for true  
+              }
+              else
+              {
+                executa se for false
+              } 
+         */
+
+      public void executar()
+      {
+        int idade = 20;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+            }
+            else 
+            {
+                Console.WriteLine("menor de idade");
+            }
+
+            double nota = 7.5;
+
+            double nota = 7.5;
+
+            if (nota >= 9)
+            {
+                Console.WriteLine("Aprovado com excelência");
+            }
+            else if (nota >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else if (nota >= 5)
+            {
+                Console.WriteLine("Recuperação");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+
+
+        }
+
+
+    }
+}
