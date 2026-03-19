829ce3220cf9a3cda3db22d51215a40042b7a1ba
GutLuiz <gutfoda@gmail.com>
2026-03-19 11:48:38 -0300

    Exercicios 1 e 2 completos

---
+++ estudos/estudos/exercicios/ex01.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos.exercicios
+{
+    internal class ex01
+    {
+        public class Pessoa {
+            private string _nome;
+            private int _idade;
+            private decimal _altura; 
+            private bool _emprego ;
+            private decimal _salario; 
+
+            public Pessoa(string nome, int idade, decimal altura, decimal salario)
+            {
+                _nome = nome;
+                _idade = idade;
+                _altura = altura;
+                _emprego = true;
+                _salario = salario;
+            }
+
+           public void Dados()
+            {
+                Console.WriteLine("Meu nome e" + _nome + "tenho " + _idade + " de idade, " + _altura + " de altura e " + _salario + " de salario");
+            }
+        }
+        public void Executar()
+        {
+            Pessoa p = new Pessoa("Gustavo", 21, 180m, 1000m);
+            p.Dados();
+        }
+
+    }
+}
---
+++ estudos/estudos/exercicios/ex02.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estudos.exercicios
+{
+    internal class ex02
+    {
+        public class Calculadora
+        {
+            private int _numero1;
+            private int _numero2;
+            private decimal _decimal1;
+            private decimal _decimal2;
+
+
+            public Calculadora(int nmr1, int nmr2, decimal dcm1, decimal dcm2)
+            {
+                _numero1 = nmr1;
+                _numero2 = nmr2;
+                _decimal1 = dcm1;
+                _decimal2 = dcm2;
+            }
+
+            public int SomarNumeros()
+            {
+                return _numero1 + _numero2;
+            }
+
+            public int SubtrairNumeros()
+            {
+                return _numero1 - _numero2;
+            }
+
+            public int MultiplicarNumeros()
+            {
+                return _numero1 * _numero2;
+            }
+
+            public decimal DividirDecimais()
+            {
+                if (_decimal2 == 0)
+                {
+                    return 0;
+                }
+                return _decimal1 / _decimal2;
+            }
+        }
+
+        public void Executar()
+        {
+            Calculadora c = new Calculadora(1, 2, 3.2m, 4.2m);
+
+            Console.WriteLine(c.SomarNumeros());
+            Console.WriteLine(c.SubtrairNumeros());
+            Console.WriteLine(c.MultiplicarNumeros());
+            Console.WriteLine(c.DividirDecimais());
+        }
+    }
+}
